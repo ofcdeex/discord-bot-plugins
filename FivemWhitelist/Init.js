89b199e7d9b331606fe5cf7aedae7a9e5617b761
deex <65043464+ofcdeex@users.noreply.github.com>
2022-09-19 16:34:18 -0300

    Update Init.js

--- FivemWhitelist/Init.js
+++ FivemWhitelist/Init.js
@@ -2,5 +2,6 @@ module.exports = {
     event: "ready",
     execute(client, func, MessageActionRow, MessageButton, PluginSettings) {
         console.log('WL LOADED');
+        console.log('WL LOADED');
     }
 }
