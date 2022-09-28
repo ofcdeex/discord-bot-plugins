1196ec8ded587bed21b2a035d12de419cc559ed3
deex <65043464+ofcdeex@users.noreply.github.com>
2022-09-28 20:06:28 -0300

    DiscordStore added

---
+++ DiscordStore/Action.js
@@ -0,0 +1,66 @@
+module.exports = {
+    event: "interactionCreate",
+    async execute(client, interaction, ActionRowBuilder, PermissionsBitField, ButtonBuilder, EmbedBuilder) {
+
+        if (!interaction.isButton()) return;
+        interaction.deferUpdate();
+
+        var config = PluginSettings.DiscordStore;
+
+
+        config.products.forEach(element => {
+            if (interaction.customId == element.id) {
+                const categorysPedidos = client.channels.cache.find(channel => channel.id == config.IdcategoriaPedidos);
+
+                const searchTicket = client.channels.cache.find(channel => channel.name === "pedido-" + interaction.user.id + `_${element.id}`);
+                if (searchTicket) {
+                    return searchTicket.send(`<@${interaction.user.id}> Aguarde a confirmação deste pedido para ralizar um novo.`);
+                }
+
+                categorysPedidos.guild.channels.create({
+                    name: 'pedido-' + interaction.user.id + `_${element.id}`,
+                    type: 0,
+                    parent: categorysPedidos,
+                    permissionOverwrites: [
+                        {
+                            id: interaction.user,
+                            allow: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                        {
+                            id: categorysPedidos.guild.roles.everyone,
+                            deny: [PermissionsBitField.Flags.ViewChannel],
+                        },
+                    ],
+                }).then((channel) => {
+
+                    /* for (var i = 0; i < settings.rolesStaff.length; i++) {
+                         var findRoles = categorysPedidos.guild.roles.cache.find(r => r.id === settings.rolesStaff[i]);
+                         channel.permissionOverwrites.edit(findRoles, [PermissionsBitField.Flags.ViewChannel]);
+                     }
+     
+                     */
+
+                    const PedidoAberto = new EmbedBuilder()
+                        .setColor(0x7289DA)
+                        .setTitle(config.loja)
+                        .setDescription(`**Pedido:** ${element.produto}\n**Valor:** R$ ${element.valor}\nChave PIX: ${config.chavepix}`);
+
+                    /*let fecharTicket = new ButtonBuilder()
+                        .setLabel("Fechar Ticket")
+                        .setEmoji("🔒")
+                        .setCustomId("close")
+                        .setStyle("Danger");
+                        */
+
+                    channel.send({
+                        embeds: [PedidoAberto]
+                        //components: [new ActionRowBuilder().addComponents(fecharTicket)]
+                    });
+
+
+                });
+            }
+        });
+
+    }
+};
---
+++ DiscordStore/Init.js
@@ -0,0 +1,34 @@
+module.exports = {
+  event: "ready",
+  execute(client, func, ActionRowBuilder, PermissionsBitField, ButtonBuilder, EmbedBuilder) {
+
+    var config = PluginSettings.DiscordStore;
+
+    config.products.forEach(element => {
+      const FindChannel = client.channels.cache.find(channel => channel.id == element.channelID);
+      var MsgDefault = new EmbedBuilder()
+        .setColor(0x7289DA)
+        .setTitle(config.loja)
+        .setDescription(`**Produto:** ${element.produto}\n**Valor:** R$ ${element.valor}\n**Descrição:**\n` + element.descricao)
+        .setImage(config.imagem)
+        .setFooter({ text: `${config.loja} todos direitos reservados.` });
+
+      let buttonsTicket = new ButtonBuilder()
+        .setLabel("Comprar")
+        .setEmoji("💰")
+        .setCustomId(element.id)
+        .setStyle("Success");
+
+      FindChannel.messages.fetch(FindChannel.lastMessageId).then(msg => {
+        // msg.edit({ embeds: [MsgDefault], components: [ButtonOpenTicket] });
+      }).catch(err => {
+        FindChannel.send({
+          embeds: [MsgDefault],
+          components: [new ActionRowBuilder().addComponents(buttonsTicket)]
+        });
+      });
+
+    });
+
+  }
+};
